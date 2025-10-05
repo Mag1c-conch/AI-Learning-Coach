8350e40c9b6c7b45ea75efd3f3fdd6c1fe50b20e
Zhennan Guo <z5499682@ad.unsw.edu.au>
2025-10-06 00:39:48 +1100

    implement user register and login routes

--- ai-learning-coach/backend/app/routes/auth.py
+++ ai-learning-coach/backend/app/routes/auth.py
@@ -1,5 +1,7 @@
 # app/routes/auth.py
 from flask import Blueprint, request, jsonify
+from ..models import User
+from ..extensions import db
 
 bp=Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -11,9 +13,41 @@ def register():
     last_name=data.get('last_name')
     username=data.get('username')
     password=data.get('password')
+    exist_user = User.query.filter_by(username=username).first()
+    if exist_user:
+        return jsonify({
+            "error": "The username has been registered!"
+        }), 201
+    user = User()
+    user.first_name = first_name
+    user.last_name = last_name
+    user.password = password
+    user.username = username
+    db.session.add(user)
+    db.session.flush()
+    db.session.refresh(user)
+    db.session.commit()
     return jsonify({
         "first_name": first_name,
         "last_name": last_name,
         "username": username,
-        "password": password
+        "id": user.id
     }), 201
+
+@bp.route('/login',methods=['POST'])
+def login():
+    data=request.json
+    username=data.get('username')
+    password=data.get('password')
+    user = User.query.filter_by(username=username, password=password).first()
+    if user:
+        return jsonify({
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "username": username,
+            "id": user.id
+        }), 201
+    else:
+        return jsonify({
+            "error": 'Invalid username or password!'
+        }), 201
