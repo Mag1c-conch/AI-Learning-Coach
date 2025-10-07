1ed527e8c2e78155165805423361a1ea2b30dc8b
Ganming Luo <z5509688@ad.unsw.edu.au>
2025-10-07 17:16:51 +1100

    update side bar before revert

--- ai-learning-coach/frontend/src/components/Sidebar.jsx
+++ ai-learning-coach/frontend/src/components/Sidebar.jsx
@@ -6,8 +6,6 @@ import LogoutIcon from '@mui/icons-material/Logout';
 const Sidebar = () => {
   return (
     <div className="wordslink">
-      {/* 中间菜单 */}
-       
       <div className="menu">
         <Button fullWidth><Link href="#" sx={{ color: '#fff', textDecoration: 'none' }}>Courses</Link></Button>
         <Button fullWidth><Link href="#" sx={{ color: '#fff', textDecoration: 'none' }}>Exercises</Link></Button>
@@ -16,7 +14,6 @@ const Sidebar = () => {
         <Button fullWidth><Link href="#" sx={{ color: '#fff', textDecoration: 'none' }}>Time Table</Link></Button>
       </div>
 
-      {/* 底部登出 */}
       <div className="logout-icon">
         <Button fullWidth>
           <Link href="/signin" sx={{ color: '#fff', display: 'flex', gap: 1, alignItems: 'center', textDecoration: 'none' }}>
