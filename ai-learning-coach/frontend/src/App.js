ba0e728714e1b91e66545315722aa113b6a784d3
小羊 <xiaoyang@xiaoyangdeMacBook-Pro.local>
2025-10-13 17:44:26 +1100

    change route

--- ai-learning-coach/frontend/src/App.js
+++ ai-learning-coach/frontend/src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signin from './components/Signin';
 import AUTH from './Constant.js';
 import Sidebar from './components/Sidebar.jsx';
-import Dashboard from './components/Dashboard.jsx';
+import Dashboard from './Student/Dashboard.jsx';
+import CoursesPage from "./Student/CoursesPage";
+import CoursePage from "./Student/CoursePage";
 import { dividerClasses } from '@mui/material';
 import AdminLayout from './Admin/AdminLayout.jsx';
 import AdminDashboard from './Admin/Pages/Admin/Dashboard.jsx';
@@ -24,6 +26,8 @@ function App() {
             <Route path="/signin" element={<Signin setToken={setToken} />} />
             <Route path="/sidebar" element={<Sidebar />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/courses" element={<CoursesPage />} />
+            <Route path="/course/:code" element={<CoursePage />} />
             <Route path="/admin" element={<AdminLayout />}>
             <Route index element={<AdminDashboard />} />
             <Route path="dashboard" element={<AdminDashboard />} />
